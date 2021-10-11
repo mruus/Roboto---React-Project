72e1c8030eea677a73ce1f1128d66b62e44f57bf
Mansuur Abdullahi Abdirahman <mansuurtech101@gmail.com>
2021-10-11 09:25:32 +0300

    MAdr

--- src/App.js
+++ src/App.js
@@ -1,50 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CardArray from "./CardArray";
 import SearchBox from "./SearchBox";
 import ErrorBoundry from "./ErrorBoundry";
 // import { robots } from "./robots";
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      searchField: "",
-    };
-  }
+function App() {
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     robots: [],
+  //     searchField: "",
+  //   };
+  // }
 
-  componentDidMount() {
+  // componentDidMount() {
+  //   fetch("https://jsonplaceholder.typicode.com/users")
+  //     .then((response) => response.json())
+  //     .then((users) => this.setState({ robots: users }));
+  // }
+
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) => this.setState({ robots: users }));
-  }
+      .then((users) => setRobots(users));
+  }, []);
 
-  onSearchange = (event) => {
-    this.setState({
-      searchField: event.target.value,
-    });
+  const [robots, setRobots] = useState([]);
+  const [searchField, setSearchField] = useState("");
+
+  const onSearchange = (event) => {
+    // this.setState({
+    //   searchField: event.target.value,
+    // });
+    setSearchField(event.target.value);
   };
 
-  render() {
-    const filteredRobots = this.state.robots.filter((robo) => {
-      return robo.name
-        .toLowerCase()
-        .includes(this.state.searchField.toLowerCase());
-    });
+  const filteredRobots = robots.filter((robo) => {
+    return robo.name
+      .toLowerCase()
+      .includes(searchField.toLowerCase());
+  });
 
-    if (this.state.robots.length === 0) {
-      return <h1>Loading...</h1>;
-    } else {
-      return (
-        <div className="tc">
-          <h1 className="title">robotfriends</h1>
-          <SearchBox searchChange={this.onSearchange} />
-          <ErrorBoundry>
-            <CardArray robots={filteredRobots} />
-          </ErrorBoundry>
-        </div>
-      );
-    }
+  if (robots.length === 0) {
+    return <h1>Loading...</h1>;
+  } else {
+    return (
+      <div className="tc">
+        <h1 className="title">robotfriends</h1>
+        <SearchBox searchChange={onSearchange} />
+        <ErrorBoundry>
+          <CardArray robots={filteredRobots} />
+        </ErrorBoundry>
+      </div>
+    );
   }
 }
 
